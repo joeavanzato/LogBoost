e5b7aea961a4dce2dcb9f6b2bb6c722ef2806469
panscan <joeavanzato@gmail.com>
2023-11-06 13:06:45 -0500

    - Improving Private IP Detection

--- main.go
+++ main.go
@@ -20,7 +20,49 @@ import (
 	"time"
 )
 
-var dnscache, _ = bigcache.New(context.Background(), bigcache.DefaultConfig(10*time.Minute))
+var cacheEntrySizeLimit = 500
+
+var bcconfig = bigcache.Config{
+	// number of shards (must be a power of 2)
+	Shards: 1024,
+
+	// time after which entry can be evicted
+	LifeWindow: 10 * time.Minute,
+
+	// Interval between removing expired entries (clean up).
+	// If set to <= 0 then no action is performed.
+	// Setting to < 1 second is counterproductive — bigcache has a one second resolution.
+	CleanWindow: 0,
+
+	// rps * lifeWindow, used only in initial memory allocation
+	MaxEntriesInWindow: 1000 * 10 * 60,
+
+	// max entry size in bytes, used only in initial memory allocation
+	// TODO - Trunce cache entries to 499 bytes to fit
+	MaxEntrySize: cacheEntrySizeLimit,
+
+	// prints information about additional memory allocation
+	Verbose: false,
+
+	// cache will not allocate more memory than this limit, value in MB
+	// if value is reached then the oldest entries can be overridden for the new ones
+	// 0 value means no size limit
+	HardMaxCacheSize: 1024,
+
+	// callback fired when the oldest entry is removed because of its expiration time or no space left
+	// for the new entry, or because delete was called. A bitmask representing the reason will be returned.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	OnRemove: nil,
+
+	// OnRemoveWithReason is a callback fired when the oldest entry is removed because of its expiration time or no space left
+	// for the new entry, or because delete was called. A constant representing the reason will be passed through.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	// Ignored if OnRemove is specified.
+	OnRemoveWithReason: nil,
+}
+
+var dnscache, _ = bigcache.New(context.Background(), bcconfig)
+var ticache, _ = bigcache.New(context.Background(), bigcache.DefaultConfig(10*time.Minute))
 
 func parseArgs(logger zerolog.Logger) (map[string]any, error) {
 	dbDir := flag.String("dbdir", "", "Directory containing existing MaxMind DB Files (if not present in current working directory")
@@ -558,9 +600,18 @@ func enrichRecord(logger zerolog.Logger, record []string, asnDB maxminddb.Reader
 			record = append(record, string(entry))
 		} else {
 			dnsRecords := lookupIPRecords(ipString)
-			ipTmpStruct.Domains = dnsRecords
+			//ipTmpStruct.Domains = dnsRecords
 			record = append(record, strings.Join(dnsRecords, "|"))
-			setdnserr := dnscache.Set(ipString, []byte(strings.Join(dnsRecords, "|")))
+			recordsJoined := strings.Join(dnsRecords, "|")
+			recordsBytes := make([]byte, 0)
+			res := []rune(recordsJoined)
+			// Naive string truncation based on ASCII mostly to fit into cache entry limit
+			if len(res) > cacheEntrySizeLimit {
+				recordsBytes = []byte(recordsJoined)[:cacheEntrySizeLimit]
+			} else {
+				recordsBytes = []byte(recordsJoined)
+			}
+			setdnserr := dnscache.Set(ipString, recordsBytes)
 			if setdnserr != nil {
 				logger.Error().Msg(setdnserr.Error())
 			}
@@ -578,15 +629,6 @@ func enrichRecord(logger zerolog.Logger, record []string, asnDB maxminddb.Reader
 		record = append(record, "")
 	}
 
-	/*	if doTorEnrich {
-			if CheckTor(ipString) {
-				record = append(record, "TRUE")
-			} else {
-				record = append(record, "FALSE")
-			}
-		} else {
-			record = append(record, "")
-		}*/
 	if useIntel {
 		matchType, TIexists, DBError := CheckIPinTI(ipString, tempArgs["db"].(*sql.DB))
 		if DBError != nil {
@@ -628,6 +670,12 @@ func main() {
 	logger := setupLogger()
 	arguments, err := parseArgs(logger)
 	if err != nil {
+		return
+	}
+
+	err = setupPrivateNetworks()
+	if err != nil {
+		logger.Error().Msg(err.Error())
 		return
 	}
 
--- network.go
+++ network.go
@@ -11,6 +11,28 @@ import (
 	"strings"
 )
 
+var privateIPBlocks []*net.IPNet
+
+func setupPrivateNetworks() error {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return err
+		}
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+	return nil
+}
+
 func downloadFile(logger zerolog.Logger, url string, filepath string, key string) (err error) {
 	if strings.HasPrefix(url, "https://download.maxmind.com") {
 		logger.Info().Msgf("Downloading MaxMind %v DB to path: %v", key, filepath)
@@ -38,6 +60,8 @@ func downloadFile(logger zerolog.Logger, url string, filepath string, key string
 }
 
 func lookupIPRecords(ip string) []string {
+	// TODO It is possible to set custom resolvers here - should explore setting up a rotating resolver to spread requests between multiple nameservers
+	// https://stackoverflow.com/questions/59889882/specifying-dns-server-for-lookup-in-go
 	records, err := net.DefaultResolver.LookupAddr(context.Background(), ip)
 	if err != nil {
 		return []string{"None"}
@@ -46,10 +70,17 @@ func lookupIPRecords(ip string) []string {
 }
 
 func isPrivateIP(ip net.IP, ipstring string) bool {
-	// TODO - Implement private net checks for IPv6
-	if tenDot.Contains(ip) || sevenTwoDot.Contains(ip) || oneNineTwoDot.Contains(ip) || ipstring == "127.0.0.1" || ipstring == "::" || ipstring == "::1" || ipstring == "0.0.0.0" {
+	if ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
 		return true
 	}
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	/*	if tenDot.Contains(ip) || sevenTwoDot.Contains(ip) || oneNineTwoDot.Contains(ip) || ipstring == "127.0.0.1" || ipstring == "::" || ipstring == "::1" || ipstring == "0.0.0.0" {
+		return true
+	}*/
 
 	return false
 }
