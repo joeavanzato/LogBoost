f6854d10518437c71975a4661ee1f72ab02557da
panscan <joeavanzato@gmail.com>
2024-01-28 19:11:57 -0500

    - Reordering columns

--- vars/vars.go
+++ vars/vars.go
@@ -47,7 +47,7 @@ var MaxMindFileLocations = map[string]string{
 // Used in func visit to add log paths as we crawl the input directory
 var LogsToProcess = make([]string, 0)
 
-var GeoFields = []string{"lb_IP", "lb_ASN", "lb_Country", "lb_City", "lb_Domains", "lb_ThreatCategory", "lb_TLD"}
+var GeoFields = []string{"lb_IP", "lb_ASN", "lb_Country", "lb_City", "lb_Domains", "lb_TLD", "lb_ThreatCategories", "lb_ThreatFeedCount", "lb_ThreatFeeds"}
 
 var Ipv6_regex = regexp.MustCompile(`.*(?P<ip>(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))).*`)
 
